3d7eeea7fc672a5d24985bf689ac387787431990
Otter <otc@otterthecat.com>
2015-08-02 17:46:01 -0500

    clean up of unused files

--- public/javascripts/components/electron.js
+++
@@ -1,7 +0,0 @@
-/*eslint-disable */
-var Electron = function(Phaser, options){
-  Phaser.Sprite.call(this, options.game, options.x, options.y, options.key, options.frame);
-};
-Electron.prototype = Object.create(Phaser.Sprite.prototype);
-Electron.prototype.constructor = Electron;
-/*eslint-enable */
--- public/javascripts/components/text.js
+++
@@ -1,6 +0,0 @@
-/*eslint-disable */
-var Text = function(Phaser, options){
-  return options.game.add.text(options.x, options.y, options.text);
-};
-
-/*eslint-enable */
